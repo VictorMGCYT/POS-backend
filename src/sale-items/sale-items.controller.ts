46677d16758e6ff5a97816c3a5c27caf11f03114
VictorMGCYT <manuelvgc2233@gmail.com>
2025-06-24 20:11:15 -0600

    Sale-Items module documented
    24-06-2025

--- src/sale-items/sale-items.controller.ts
+++ src/sale-items/sale-items.controller.ts
@@ -2,12 +2,22 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from
 import { SaleItemsService } from './sale-items.service';
 import { CreateSaleItemDto } from './dto/create-sale-item.dto';
 import { UpdateSaleItemDto } from './dto/update-sale-item.dto';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('sale-items')
 export class SaleItemsController {
   constructor(private readonly saleItemsService: SaleItemsService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener items vendidos', description: 'Detalles de todos los items vendidos en una venta' })
+  @ApiResponse({ status: 200, description: 'Items de venta obtenidos exitosamente' })
+  @ApiResponse({ status: 404, description: 'Items de venta no encontrados'})
+  @ApiParam({ 
+    name: 'id',
+    description: 'ID de la venta para obtener sus items',
+    type: String,
+    example: 'bda4f20a-c044-494e-b172-7f9a17562478'
+  })
   findBySaleId(@Param('id', ParseUUIDPipe) id: string) {
     return this.saleItemsService.findBySaleId(id);
   }
